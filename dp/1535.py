a2074b8fc7b4885ffaf5b06b05a222bad01609ed
chrconst <charconst05@gmail.com>
2025-11-21 01:00:02 +0900

    2025-11-21 01:00:02

--- dp/1535.py
+++ dp/1535.py
@@ -1,4 +1,9 @@
-1 ~ n
-L[i] - 체력
-J[i] + 기쁨
-visited
+n = int(input())
+L = list(map(int, input().split()))
+J = list(map(int, input().split()))
+dp = [0]*100
+
+for i in range(len(L)):
+    for o in range(99, L[i]-1, -1):
+        dp[o] = max(dp[o], dp[o-L[i]]+J[i])
+print(dp[-1])
