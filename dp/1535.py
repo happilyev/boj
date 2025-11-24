9918b7f6b86ea175b6a825481ba3a5ce7177a386
chrconst <charconst05@gmail.com>
2025-11-25 01:00:12 +0900

    2025-11-25 01:00:12

--- dp/1535.py
+++ dp/1535.py
@@ -1,11 +1,13 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-
 L = list(map(int, input().split()))
-J = list(map(int, input.split()))
+J = list(map(int, input().split()))
 
 hp = [0]*100
 
 
-
+for i in range(len(L)):
+    for o in range(99, L[i]-1, -1):
+        hp[o] = max(hp[o],hp[o-L[i]]+J[i])
+print(hp[-1])
